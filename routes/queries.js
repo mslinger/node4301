be338b5995ed9a992e24f3cd173e041e1cbc127c
Matthew <mdenslinger@gmail.com>
2021-11-29 14:51:53 -0500

    Add additional query, some ejs logic, and html

--- routes/queries.js
+++ routes/queries.js
@@ -26,10 +26,64 @@ router.post('/queryone', async (req, res, next) => {
     }
     const result = await query(statement);
 
-    res.render('queryone.ejs', {pagetitle: "Flix - Query One", year: input_year, data: result}); 
+    res.render('queryone.ejs', {pagetitle: "Flix - Query One", year: input_year, data: result, first_query: true}); 
 });
 
-router.get('/querytwo', (req, res, next) => {
+router.post('/queryonep2', async (req, res, next) => {
+    
+    const from_year = req.body.fromyear;
+    const to_year = req.body.toyear;
+    const genres = req.body.genres;
+    
+    let years = [];    
+
+    let final_genres = [];
+
+    let where_clause = ``;
+
+    for(let i=0; i < genres.length; i++){
+
+        final_genres.push(genres[i]);
+        
+        if(i == 5 | i == (genres.length-1)){
+            where_clause += `GenreType = '${genres[i]}'`;
+            break;            
+        }
+        else{
+            where_clause += `GenreType = '${genres[i]}' OR `;
+        }        
+    }
+    
+    let statement = ``;
+
+    for(let i=from_year; i <= to_year; i++){
+
+        years.push(i);
+        
+        if(i == to_year){            
+            statement += `SELECT Year, GenreType, COUNT(GenreType)/(SELECT COUNT(GenreType) FROM Genre JOIN Movie ON Genre.MovieID = Movie.ID WHERE Year = ${i})*100 AS Total 
+            FROM Genre JOIN Movie on Movie.ID = Genre.MovieID WHERE Year = ${i}
+            GROUP BY GenreType, Year`;
+        }
+        else{            
+            statement += `SELECT Year, GenreType, COUNT(GenreType)/(SELECT COUNT(GenreType) FROM Genre JOIN Movie ON Genre.MovieID = Movie.ID WHERE Year = ${i})*100 AS Total 
+                FROM Genre JOIN Movie on Movie.ID = Genre.MovieID WHERE Year = ${i} 
+                GROUP BY GenreType, Year`;
+            statement += ` UNION `;
+        }   
+    }
+    
+    statement = `SELECT Year, GenreType, Total FROM(` + statement + `) WHERE ${where_clause} ORDER BY GenreType ASC, YEAR ASC`;
+    
+    for(let i= 0; i < years.length; i++){
+        years[i] = years[i].toString();
+    }
+    const result = await query(statement);    
+
+    res.render('queryone.ejs', {pagetitle: "Flix - Query One", years: years, data: result, genres: final_genres, second_query: true}); 
+});
+
+router.get('/querytwo', async (req, res, next) => {
     res.render('querytwo.ejs', {pagetitle: "Flix - Query Two"}); 
 });
 
